a4fd11345d6ad72d5b01da01e4099a050ca5895a
İhsan <altnskihsan@gmail.com>
2023-11-24 13:31:04 +0300

    11.24.23

--- src/test/java/day08_iFrame_cokluWindows/C03_iFrame.java
+++ src/test/java/day08_iFrame_cokluWindows/C03_iFrame.java
@@ -1,6 +1,11 @@
 package day08_iFrame_cokluWindows;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utilities.testBase;
+
+import java.util.List;
 
 public class C03_iFrame extends testBase {
     // 1- Go to http://demo.guru99.com/test/guru99home/.
@@ -8,6 +13,26 @@ public class C03_iFrame extends testBase {
     // 3- Find the number of iframes on the page.
     // 4- Click on the play button in the first iframe (Youtube).
     // 5- Exit the first iframe and return to the homepage.
-    // 6- Click on the link (https://www.guru99.com/live-selenium-project.html) in the second iframe (Jmeter Made Easy).
+    @Test
+    public void test01(){
+        /// 1- Go to http://demo.guru99.com/test/guru99home/.
+        driver.get("http://demo.guru99.com/test/guru99home/");
 
+        // 2- Accept the cookies.
+        WebElement cookiesIFrame = driver.findElement(By.xpath("(//iframe)[4]"));
+        driver.switchTo().frame(cookiesIFrame);
+        driver.findElement(By.xpath("//*[text()='Accept All']")).click();
+
+        // 3- Find the number of iframes on the page.
+        List<WebElement> iFrameList = driver.findElements(By.tagName("iframe"));
+        System.out.println("Number of iFrame : " + iFrameList.size());
+
+        // 4- Click on the play button in the first iframe (Youtube).
+        WebElement playIFrame = driver.findElement(By.xpath("(//iframe)[4]"));
+        driver.switchTo().frame(playIFrame);
+        driver.findElement(By.xpath("//button[@title='Play']")).click();
+
+        // 5- Exit the first iframe and return to the homepage.
+        driver.switchTo().defaultContent();
+    }
 }
