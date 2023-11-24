3fa08f76b8c79cb85efeec78ccd8c86d52d21f53
İhsan <altnskihsan@gmail.com>
2023-11-24 14:15:24 +0300

    11.24.23

---
+++ src/test/java/day08_iFrame_cokluWindows/C04_.java
@@ -0,0 +1,58 @@
+package day08_iFrame_cokluWindows;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WindowType;
+import utilities.testBase;
+
+public class C04_ extends testBase {
+    // 1- Go to the Testotomasyonu homepage.
+    // 2- Test that the URL contains 'testotomasyonu'.
+    // 3- Open the 'Electronics' link in a new tab.
+    // 4- Test that the opened page is the Electronics page.
+    // 5- Click on the 'Men's Fashion' link to open it in a new window.
+    // 6- Test that the Men's Fashion page is opened.
+    // 7- Return to the initially opened page.
+    // 8- Test that we are back on the Testotomasyonu homepage.
+
+    @Test
+    public void controlledWindowTest(){
+        // 1- Go to the Testotomasyonu homepage.
+        driver.get("https://testotomasyonu.com");
+
+        // 2- Test that the URL contains 'testotomasyonu'.
+        String expectedURL ="testotomasyonu";
+        String actualURL= driver.getCurrentUrl();
+        Assert.assertTrue(actualURL.contains(expectedURL));
+        String mainWHD = driver.getWindowHandle();
+
+        // 3- Open the 'Electronics' link in a new tab.
+        driver.switchTo().newWindow(WindowType.TAB).get("https://testotomasyonu.com");
+        driver.findElement(By.linkText("Electronics")).click();
+        String electronicsWHD = driver.getWindowHandle();
+
+        // 4- Test that the opened page is the Electronics page.
+        String expectedTitle = "Electronics";
+        String actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+        // 5- Click on the 'Men's Fashion' link to open it in a new window.
+        driver.switchTo().newWindow(WindowType.WINDOW).get("https://www.testotomasyonu.com");
+        driver.findElement(By.xpath("(//a[text()='Men Fashion'])[3]")).click();
+        String menFashionWHD = driver.getWindowHandle();
+
+        /// 6- Test that the Men's Fashion page is opened.
+        expectedTitle = "Men Fashion";
+        actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+        // 7- Return to the initially opened page.
+        driver.switchTo().window(mainWHD);
+
+        // 8- Test that we are back on the Testotomasyonu homepage.
+        String expectedUrl = "https://www.testotomasyonu.com/";
+        actualURL = driver.getCurrentUrl();
+        Assert.assertEquals(expectedUrl,actualURL);
+    }
+}
---
+++ src/test/java/day08_iFrame_cokluWindows/C05_.java
@@ -0,0 +1,12 @@
+package day08_iFrame_cokluWindows;
+
+public class C05_ {
+    // 1- Go to https://testotomasyonu.com/addremove/.
+    // 2- Verify that the text on the page is "Add/Remove Elements".
+    // 3- Verify that the page title is "Test Otomasyonu".
+    // 4- Click on the 'Please click for Electronics Products' link.
+    // 5- Test that the Electronics page is opened.
+    // 6- Test that the number of found products is 16.
+    // 7- Return to the initially opened addremove page.
+    // 8- Test that the URL contains 'addremove'.
+}
