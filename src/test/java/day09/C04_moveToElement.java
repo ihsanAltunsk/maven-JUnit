d0f2a68900bbb5c1bd194eab24d22649f5db299f
İhsan <altnskihsan@gmail.com>
2023-11-29 11:31:53 +0300

    11.29.23

---
+++ src/test/java/day09/C04_moveToElement.java
@@ -0,0 +1,15 @@
+package day09;
+
+public class C04_moveToElement {
+    // 1- Go to https://www.testotomasyonu.com/
+    // 2- Hover the mouse over the "Kids Wear" menu to open it
+    // 3- Click on the "Boys" link
+    // 4- Click on the first product on the opened page
+    // 5- Test that the product name on the opened page is "Boys Shirt White Color"
+
+
+
+
+
+
+}
