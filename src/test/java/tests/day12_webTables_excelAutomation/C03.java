96ba42dcc4eb6de977059f65760d247b1907a425
İhsan <altnskihsan@gmail.com>
2023-12-03 12:40:18 +0300

    12.03.23

--- src/test/java/tests/day12_webTables_excelAutomation/C03.java
+++
@@ -1,4 +0,0 @@
-package tests.day12_webTables_excelAutomation;
-
-public class C03 {
-}
---
+++ src/test/java/tests/day12_webTables_excelAutomation/C03_ReadExcelTest.java
@@ -0,0 +1,23 @@
+package tests.day12_webTables_excelAutomation;
+
+import org.junit.Test;
+
+public class C03_ReadExcelTest {
+    // 1- Let's go to the 2nd cell in the 1st row and print its content.
+    // 2- Assign the content of the 2nd cell in the 1st row to a string variable and print it.
+    // 3- Test if the 4th cell in the 2nd row states the capital of Afghanistan.
+    // 4- Find the total number of rows.
+    // 5- Find the number of physically used rows.
+    // 6- Save English country names and their capitals as a map.
+
+    @Test
+    public void test01(){
+        // 1- Let's go to the 2nd cell in the 1st row and print its content.
+
+        // 2- Assign the content of the 2nd cell in the 1st row to a string variable and print it.
+        // 3- Test if the 4th cell in the 2nd row states the capital of Afghanistan.
+        // 4- Find the total number of rows.
+        // 5- Find the number of physically used rows.
+        // 6- Save English country names and their capitals as a map.
+    }
+}
