bd9a8caa094d7e9510b424d4b422f6244a087cb7
İhsan <altnskihsan@gmail.com>
2023-12-03 13:17:21 +0300

    12.03.23

--- src/test/java/tests/day12_webTables_excelAutomation/C03_ReadExcelTest.java
+++ src/test/java/tests/day12_webTables_excelAutomation/C03_ReadExcelTest.java
@@ -1,6 +1,5 @@
 package tests.day12_webTables_excelAutomation;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
@@ -16,15 +15,18 @@ public class C03_ReadExcelTest {
     // 3- Test if the 4th cell in the 2nd row states the capital of Afghanistan.
     // 4- Find the total number of rows.
     // 5- Test if the capital city of Samoa is named Apia.
-    // 6- Find the number of physically used rows.
-    // 7- Save English country names and their capitals as a map.
+    // 6- Create a method that, based on the English country name
+    //    and language preference we provide, returns the capital city.
+    // 7- Find the number of physically used rows.
+    // 8- Save English country names and their capitals as a map.
 
+    Sheet page1;
     @Test
     public void test01() throws IOException {
         String filePath = "src/test/java/tests/day12_webTables_excelAutomation/ulkeler.xlsx";
         FileInputStream fileInputStream = new FileInputStream(filePath);
         Workbook workbook = WorkbookFactory.create(fileInputStream);
-        Sheet page1 = workbook.getSheet("Sayfa1");
+        page1 = workbook.getSheet("Sayfa1");
 
         // 1- Let's go to the 2nd cell in the 1st row and print its content.
         System.out.println(page1.getRow(0).getCell(1));
@@ -51,11 +53,29 @@ public class C03_ReadExcelTest {
         }
         Assert.assertEquals(expectedCapital,actualCapital);
 
-        // 6- Find the number of physically used rows.
+        // 6- Create a method that, based on the English country name
+        //    and language preference we provide, returns the capital city.
+        System.out.println(findCapital("fransa"));
+        // 7- Find the number of physically used rows.
 
 
-        // 7- Save English country names and their capitals as a map.
+        // 8- Save English country names and their capitals as a map.
 
 
+    }
+    public String findCapital (String countryName, String language){
+        String countryNameOnRow;
+        String capitalName ="";
+        for (int i = 0; i <= page1.getLastRowNum() ; i++) {
+            countryNameOnRow = page1.getRow(i).getCell(0).toString();
+            if (countryNameOnRow.equalsIgnoreCase(countryName)){
+                if (language.equalsIgnoreCase("English")){ // ingilizce baskent
+                    capitalName = page1.getRow(i).getCell(1).toString();
+                }else{
+                    capitalName = page1.getRow(i).getCell(3).toString();
+                }
+            }
+        }
+        return capitalName;
     }
 }
